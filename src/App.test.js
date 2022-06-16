0f044bd0fde2e30e2a592bab55937c037f65ce1a
fadel100 <fadel.ibrahim.it@gmail.com>
2022-06-16 23:15:01 +0300

    fix test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn hjssadasdasdasdadadada/i);
+  const linkElement = screen.getByText(/learn hj/i);
   expect(linkElement).toBeInTheDocument();
 });
 
